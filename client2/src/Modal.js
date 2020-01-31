0234ebefedfada29c22411dfae1eb20dd37538a8
Sergey.Chukanov <schukanov@gmail.com>
2020-01-31 21:34:49 +0300

    Login modal

--- client2/src/Modal.js
+++ client2/src/Modal.js
@@ -8,18 +8,36 @@ function Modal({active, close}) {
                 <div className="modal-background"></div>
                 <div className="modal-card">
                     <header className="modal-card-head">
-                        <p className="modal-card-title">Modal title</p>
+                        <p className="modal-card-title">Login</p>
                         <button className="delete" aria-label="close" onClick={ close }></button>
                     </header>
                     <section className="modal-card-body">
                         <div className="content">
 
+                            <div className="field is-horizontal">
+                                <div className="field-body">
+                                    <div className="field">
+                                        <p className="control">
+                                            <input className="input" type="email" placeholder="Login"/>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="field is-horizontal">
+                                <div className="field-body">
+                                    <div className="field">
+                                        <p className="control">
+                                            <input className="input" type="password" placeholder="Password"/>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
 
                         </div>
                     </section>
                     <footer className="modal-card-foot">
-                        <button className="button is-success">Save changes</button>
-                        <button className="button">Cancel</button>
+                        <button className="button is-success">Login</button>
+                        <button className="button" onClick={close}>Cancel</button>
                     </footer>
                 </div>
             </div>
